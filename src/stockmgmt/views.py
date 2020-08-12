2c0699d854144f94c2df322421bd3842e3e63d4f
rrico321 <robertrico15@hotmail.com>
2020-08-12 14:29:55 -0400

    first push

---
+++ src/stockmgmt/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockCreateForm, StockSearchForm
+
+
+# Create your views here.
+def home(request):
+	title = 'Welcome: This is the Home Page!'
+	form = 'Welcome: This is the Home Page!'
+	context = {
+		"title": title,
+		"test": form,
+	}
+	return render(request, "home.html", context)
+
+
+def list_items(request):
+	header = 'Inventory'
+	form = StockSearchForm(request.POST or None)
+	queryset = Stock.objects.all()
+	context = {
+		"header": header,
+		"queryset": queryset,
+		"form": form,
+	}
+	if request.method == 'POST':
+		queryset = Stock.objects.filter(category__icontains=form['category'].value(),
+										item_name__icontains=form['item_name'].value()
+										)
+		context = {
+		"form": form,
+		"header": header,
+		"queryset": queryset,
+}
+	return render(request, "list_items.html", context)
+
+def add_items(request):
+	form = StockCreateForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('/list_items')
+	context = {
+		"form": form,
+		"title": "Add Item",
+	}
+	return render(request, "add_items.html", context)
+
+
+
